f741a2d73ca9cba1147de19e3be2ba4717d5fbf5
Bhavesh Patel <b.patel@berkeley.edu>
2021-08-25 21:39:32 -0700

    Update TheTeam.vue

--- src/views/team/TheTeam.vue
+++ src/views/team/TheTeam.vue
@@ -60,7 +60,7 @@ export default {
           id: "BhaveshPatel",
           name: "Bhavesh Patel (he/him)",
           title: "Founder/Lead",
-          bio: "Bhavesh hails from Thiais, France. He specializes in modeling (mathematical) and computational simulations. He is also learning software development so he can criticize his team's work. He is passionate about soccer and vegan croissants.",
+          bio: "Bhavesh hails from Thiais, France. His expertize lies in modeling (mathematical) and computational simulations. He has also established significant knowledge of software development so he can accurately criticize his team's work. He is passionate about soccer and vegan croissants.",
           image:
             "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
           borderTop: false,
