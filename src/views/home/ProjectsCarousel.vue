62ec88c49e52f6a4b9c893a16a31aa3ded48cc71
Sanjay Soundarajan <sanjay.16@live.com>
2021-08-25 17:19:15 -0700

    Added new carousel library

--- src/views/home/ProjectsCarousel.vue
+++ src/views/home/ProjectsCarousel.vue
@@ -164,22 +164,165 @@
         </template>
       </vueper-slide>
     </vueper-slides>
+    <Flicking
+      :options="{ circular: true, align: 'center', horizontal: true }"
+      :plugins="plugins"
+    >
+      <section
+        v-for="project in projectsList"
+        :key="project.name"
+        class="
+          flex flex-col
+          text-gray-600
+          w-full
+          h-full
+          body-font
+          px-5
+          py-5
+          flex
+          justify-center
+        "
+      >
+        <div
+          class="
+            container
+            mx-auto
+            flex
+            w-4/5
+            sm:mb-5
+            flex-col-reverse
+            items-center
+          "
+        >
+          <!-- text div -->
+          <div
+            class="flex flex-col my-4 sm:mb-16 md:mb-0 items-center text-center"
+          >
+            <!-- <h3
+                    class="
+                      font-inter font-semibold
+                      text-3xl
+                      md:text-2xl
+                      lg:text-3xl
+                      w-full
+                      mb-1
+                      sm:mb-4
+                      text-left
+                      sm:text-center
+                      dark:text-gray-50
+                    "
+                  >
+                    KnowMore
+                  </h3> -->
+
+            <p
+              class="
+                w-full
+                text-left
+                sm:text-center
+                text-base
+                md:text-base
+                lg:text-lg
+                font-asap
+                text-black
+                dark:text-white
+              "
+            >
+              {{ project.description }}
+            </p>
+
+            <div class="w-full flex justify-center py-4">
+              <router-link :to="project.page">
+                <button
+                  class="
+                    sm:ml-4
+                    inline-flex
+                    text-white
+                    border-0
+                    py-2
+                    px-6
+                    focus:outline-none
+                    rounded
+                    text-lg
+                    md:text-base
+                    lg:text-lg
+                    transition
+                    bg-black
+                    dark:bg-pink-600
+                    text-white
+                    ring-2 ring-offset-2 ring-transparent
+                    dark:ring-offset-transparent
+                    hover:ring-pink-600
+                    dark:hover:ring-offset-1 dark:hover:ring-white
+                    focus:ring-pink-600
+                  "
+                >
+                  Learn more about {{ project.name }}
+                </button>
+              </router-link>
+            </div>
+          </div>
+          <!-- image div -->
+          <div
+            class="
+              lg:max-w-lg
+              my-5
+              sm:py-0
+              flex flex-row
+              justify-center
+              items-center
+            "
+          >
+            <img
+              class="
+                object-cover object-center
+                rounded
+                sm:pt-20
+                md:pt-0
+                sm:h-full
+                md:h-auto
+                lg:h-auto
+                sm:w-60
+                md:w-72
+                lg:w-80
+              "
+              alt="SODA for SPARC logo"
+              :src="project.imageUrl"
+            />
+          </div>
+        </div>
+      </section>
+      <template #viewport>
+        <div class="flicking-pagination"></div>
+      </template>
+    </Flicking>
   </div>
 </template>
 
 <script>
 import { VueperSlides, VueperSlide } from "vueperslides";
 import "vueperslides/dist/vueperslides.css";
+import Flicking from "@egjs/vue3-flicking";
+import "@egjs/vue3-flicking/dist/flicking.css";
+import "@egjs/flicking-plugins/dist/flicking-plugins.css";
+
+import { AutoPlay, Pagination } from "@egjs/flicking-plugins";
 
 export default {
   name: "ProjectsCarousel",
   components: {
     VueperSlides,
     VueperSlide,
+    Flicking,
   },
   props: ["projectsList"],
   data() {
-    return {};
+    return {
+      plugins: [
+        new AutoPlay({ duration: 2000, direction: "NEXT", stopOnHover: true }),
+        new Pagination({ type: "scroll" }),
+      ],
+    };
   },
   computed: {
     getCarouselHeight: function () {
