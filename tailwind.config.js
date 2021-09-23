ee676744a9cade15572a99ea00ce5349d1ad3fb2
Sanjay Soundarajan <sanjay.16@live.com>
2021-09-23 15:22:28 -0700

    Added Sitemap

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         white:
           "0 10px 15px -3px rgba(255,255,255, 0.1),  0 4px 6px -2px rgba(255,255,255, 0.05)",
       },
-      backgroundImage: (theme) => ({
+      backgroundImage: () => ({
         haikeiWavy: "url('./assets/backgrounds/wavy-background.svg')",
         haikeiCircleScatter:
           "url('./assets/backgrounds/circle-scatter-haikei.svg')",
---
+++ vue.config.js
@@ -0,0 +1,50 @@
+// vue.config.js
+
+module.exports = {
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        {
+          loc: "https://fairdataihub.org/",
+          priority: 0.8,
+          changefreq: "weekly",
+        },
+        {
+          loc: "https://fairdataihub.org/team",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/contactus",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/sodaforsparc",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/sodaforsparc/docs",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/knowmore",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/sparclink",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+        {
+          loc: "https://fairdataihub.org/aqua",
+          priority: 0.8,
+          changefreq: "monthly",
+        },
+      ],
+    },
+  },
+};
