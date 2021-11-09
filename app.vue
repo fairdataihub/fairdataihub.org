14970e0e496e5da8f860b688d2d7f697caa2de7b
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-11-09 01:44:02 -0800

    Update for mobile viewport

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@ export default {
         lang: "en-US",
       },
       meta: [
-        { name: "author", content: "Jeffrey Harmon" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
           name: "description",
           content:
