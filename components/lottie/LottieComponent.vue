3580e455cf9329359bec51cff2f783bcb1329e2a
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-01-15 23:42:15 -0800

    Wait for lottie component

---
+++ components/lottie/LottieComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="LottieComponent"></div>
+</template>
+
+<script>
+import lottie from "lottie-web";
+
+export default {
+  props: {
+    lottieAnimationData: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {},
+  mounted() {
+    let interval = setInterval(() => {
+      const element = this.$refs.LottieComponent;
+
+      if (element !== null) {
+        clearInterval(interval);
+
+        lottie.loadAnimation({
+          container: element,
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+          animationData: this.lottieAnimationData,
+        });
+      }
+    }, 100);
+  },
+};
+</script>
+
+<style></style>
