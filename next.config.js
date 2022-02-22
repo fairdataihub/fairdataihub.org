45faf1fe235f0450dc36b3002832b0d8e79d3109
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-02-21 23:58:45 -0800

    update image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['www.datocms-assets.com'],
+    domains: [
+      'www.datocms-assets.com',
+      'github.com',
+      'ucarecdn.com',
+      'avatars.dicebear.com',
+      'img.shields.io',
+      'researcherprofiles.org',
+    ],
   },
 };
