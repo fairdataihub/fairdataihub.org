0d99d9c37d74b95613b28dd893994beb850db3c0
Sanjay Soundarajan <sanjay.16@live.com>
2022-06-30 14:15:41 -0700

    feat: ✨ add meta tags and adjust design

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -4,81 +4,159 @@ import dayjs from 'dayjs';
 
 import { Icon } from '@iconify/react';
 
+import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import markdownToHtml from '@/lib/markdownToHtml';
+
+import authorsList from '@/assets/data/authors.json';
 
 import PostBody from '@/components/blog/postBody';
 
 // The page for each post
-export default function Post({ slug, frontmatter, postContent }) {
-  const { title, author, date, heroImage } = frontmatter;
+export default function Post({ slug, frontMatter, postContent }) {
+  const { title, authors, date, heroImage, category, subtitle, tags } =
+    frontMatter;
 
   return (
-    <div className="relative mx-auto flex h-full w-full max-w-screen-lg flex-col  overflow-hidden  px-5   sm:py-10 sm:px-10 ">
-      <div className="mb-10">
-        <Image
-          src={heroImage}
-          alt={title}
-          width="200%"
-          height="100%"
-          layout="responsive"
-          objectFit="cover"
+    <div className="relative mx-auto flex h-full w-full max-w-screen-xl flex-col justify-center sm:flex-row">
+      <Head>
+        <title>{`${title} - Blog | Fair Data Innovations Hub`}</title>
+        <meta
+          property="og:title"
+          content={`${title} - Blog | Fair Data Innovations Hub`}
         />
-      </div>
-      <div className=" ">
-        <h3 className="mb-3  text-base font-medium text-slate-500">
-          {dayjs(date).format('dddd, MMMM D, YYYY')}
-        </h3>
+        <meta
+          property="twitter:title"
+          content={`${title} - Blog | Fair Data Innovations Hub`}
+        />
 
-        <h1 className="text-5xl font-extrabold text-slate-700">{title}</h1>
+        <link rel="canonical" href={`https://fairdataihub.org/blog/${slug}`} />
+        <meta
+          property="og:url"
+          content={`https://fairdataihub.org/blog/${slug}`}
+        />
+        <meta
+          property="twitter:url"
+          content={`https://fairdataihub.org/blog/${slug}`}
+        />
 
-        <div className="flex items-center pt-6 pb-3">
-          <div className="mr-3 flex items-center">
-            <Image
-              src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              alt="profile picture"
-              width={50}
-              height={50}
-              className="rounded-full"
-              objectFit="cover"
-            />
-          </div>
-          <div className="flex flex-col justify-center text-base">
-            <div className="text">{author}</div>
+        <meta name="description" content={subtitle} />
+        <meta property="og:description" content={subtitle} />
+        <meta property="twitter:description" content={subtitle} />
+
+        <meta
+          property="og:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+
+        <meta
+          name="keywords"
+          content={tags.map((tag) => tag.toLowerCase()).join(', ')}
+        />
+      </Head>
+
+      <Link href="/blog" passHref>
+        <div className="text-url top-0 left-0 my-4 mx-4 cursor-pointer py-1 text-[14px] font-semibold hover:underline sm:absolute sm:text-base">
+          <div className="flex items-center justify-center">
+            <Icon icon="eva:arrow-ios-back-fill" /> Go back
           </div>
         </div>
+      </Link>
 
-        <PostBody content={postContent} />
+      <div className="relative mx-auto flex h-full w-full max-w-screen-lg flex-col overflow-hidden py-5 px-5 sm:py-20 sm:px-10 ">
+        <div className="mb-10">
+          <Image
+            src={heroImage}
+            alt={title}
+            width="200%"
+            height="100%"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
+        <div className=" ">
+          <h3 className="mb-2 text-lg font-bold text-accent">{category}</h3>
+          <h3 className="mb-3 text-base font-medium text-slate-500">
+            {dayjs(date).format('dddd, MMMM D, YYYY')}
+          </h3>
 
-        <div className="flex flex-row items-center">
-          <span className="mr-2 text-sm text-slate-600">
-            Share this article:{' '}
-          </span>
-          <a
-            href={`http://twitter.com/share?text=I just read this article and think y'all need to take a look at this&url=https://fairdataihub.org/blog/${slug}&hashtags=FAIRData,OpenScience,OpenSource`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-1 text-slate-500 transition-all hover:text-accent"
-          >
-            <Icon icon="akar-icons:twitter-fill" />
-          </a>
-          <a
-            href={`https://www.facebook.com/sharer/sharer.php?u=https://fairdataihub.org/blog/${slug}"`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-1 text-slate-500 transition-all hover:text-accent"
-          >
-            <Icon icon="akar-icons:facebook-fill" />
-          </a>
-          <a
-            href={`https://www.linkedin.com/sharing/share-offsite/?url=https://fairdataihub.org/blog/${slug}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-1 text-slate-500 transition-all hover:text-accent"
-          >
-            <Icon icon="akar-icons:linkedin-box-fill" />
-          </a>
+          <hr className="my-4 border-dashed border-slate-200" />
+
+          <h1 className="text-5xl font-extrabold text-slate-700">{title}</h1>
+
+          <ul className="flex flex-wrap pt-6 pb-3 text-sm leading-6 ">
+            {authors.map((author) => (
+              <li key={author} className="my-2 mr-5 flex items-center">
+                <Image
+                  src={authorsList[author].avatar}
+                  alt="profile picture"
+                  width={50}
+                  height={50}
+                  className=" flex items-center rounded-full"
+                  objectFit="cover"
+                />
+
+                <div className="ml-3 flex flex-col justify-center">
+                  <span className="text-base font-medium ">
+                    {authorsList[author].name}
+                  </span>
+                  <a
+                    href={'https://twitter.com/' + authorsList[author].social}
+                    className="text-sm font-medium text-accent"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    @{authorsList[author].social}
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <hr className="my-2 border-slate-300" />
+
+          <PostBody content={postContent} />
+
+          <hr className="my-3 border-slate-300" />
+
+          <div className="mb-10 flex flex-row items-center">
+            <span className="mr-2 text-base text-slate-600">
+              Share this article:{' '}
+            </span>
+            <a
+              href={`http://twitter.com/share?text=I just read this article and think y'all need to take a look at this&url=https://fairdataihub.org/blog/${slug}&hashtags=FAIRData,OpenScience,OpenSource`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-1 text-slate-500 transition-all hover:text-accent"
+              aria-label="Share on Twitter"
+            >
+              <Icon icon="akar-icons:twitter-fill" />
+            </a>
+            <a
+              href={`https://www.facebook.com/sharer/sharer.php?u=https://fairdataihub.org/blog/${slug}"`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-1 text-slate-500 transition-all hover:text-accent"
+              aria-label="Share on Facebook"
+            >
+              <Icon icon="akar-icons:facebook-fill" />
+            </a>
+            <a
+              href={`https://www.linkedin.com/sharing/share-offsite/?url=https://fairdataihub.org/blog/${slug}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-1 text-slate-500 transition-all hover:text-accent"
+              aria-label="Share on LinkedIn"
+            >
+              <Icon icon="akar-icons:linkedin-box-fill" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -102,14 +180,14 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params: { slug } }) {
   const fileName = fs.readFileSync(`blog/${slug}.md`, 'utf-8');
-  const { data: frontmatter, content: fileContent } = matter(fileName);
+  const { data: frontMatter, content: fileContent } = matter(fileName);
 
   const postContent = await markdownToHtml(fileContent || '');
 
   return {
     props: {
       slug,
-      frontmatter,
+      frontMatter,
       postContent,
     },
   };
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,15 +1,52 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import wordsCount from 'words-count';
+
 import Link from 'next/link';
+import Head from 'next/head';
 
 import dayjs from 'dayjs';
 
 // The Blog Page Content
 export default function Blog({ blogList }) {
   return (
-    <section className="relative mx-auto flex h-full w-full max-w-screen-lg flex-col  overflow-hidden  px-5   sm:py-10 sm:px-10">
-      <div className="mb-10">
+    <section className="relative mx-auto flex h-full w-full max-w-screen-xl flex-col  overflow-hidden  px-5   sm:py-10 sm:px-10">
+      <Head>
+        <title>Fair Data Innovations Hub</title>
+        <meta property="og:title" content="Blog - Fair Data Innovations Hub " />
+        <meta
+          property="twitter:title"
+          content="Blog - Fair Data Innovations Hub"
+        />
+
+        <link rel="canonical" href="https://fairdataihub.org/blog" />
+        <meta property="og:url" content="https://fairdataihub.org/blog" />
+        <meta property="twitter:url" content="https://fairdataihub.org/blog" />
+
+        <meta
+          name="description"
+          content="Updates from FAIR Data Innovations Hub and its team."
+        />
+        <meta
+          property="og:description"
+          content="Updates from FAIR Data Innovations Hub and its team."
+        />
+        <meta
+          property="twitter:description"
+          content="Updates from FAIR Data Innovations Hub and its team."
+        />
+
+        <meta
+          property="og:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+      </Head>
+
+      <div className="mb-10 pt-5 sm:pt-0">
         <h1 className="mb-2 text-center text-4xl font-bold sm:text-4xl">
           Latest Updates
         </h1>
@@ -20,9 +57,9 @@ export default function Blog({ blogList }) {
       </div>
 
       {blogList.map((post) => {
-        const { slug, frontmatter, timeToRead } = post;
+        const { slug, frontMatter, timeToRead } = post;
 
-        const { title, date, tags, subtitle } = frontmatter;
+        const { title, date, tags, subtitle, category } = frontMatter;
 
         return (
           <article
@@ -35,33 +72,43 @@ export default function Blog({ blogList }) {
                 {dayjs(date).format('MMMM D, YYYY')}
               </h3>
 
+              <h4 className="text-url mb-1 cursor-pointer text-base font-semibold hover:underline">
+                {category}
+              </h4>
+
               <span className="text-sm text-gray-600">
                 {timeToRead} min read
               </span>
             </div>
 
-            <div className="flex  flex-col rounded-lg px-2 py-10 transition-all hover:bg-stone-100 hover:shadow-sm md:w-8/12 md:py-5 md:px-7">
+            <div className="flex flex-col rounded-lg px-2 py-7 transition-all hover:bg-stone-100 hover:shadow-sm md:w-8/12 md:py-5 md:px-7">
+              <h3 className="text-url mb-1 cursor-pointer text-base font-semibold hover:underline md:hidden">
+                {category}
+              </h3>
+
               <div className="mb-1 flex flex-row items-center justify-between md:hidden ">
-                <h3 className="  text-base font-medium text-slate-600 ">
+                <h4 className="text-base font-medium text-slate-600 ">
                   {dayjs(date).format('MMMM D, YYYY')}
-                </h3>
+                </h4>
 
                 <span className=" text-sm text-gray-600">
                   {timeToRead} min read
                 </span>
               </div>
 
+              <hr className="my-1 border-dashed border-slate-200 md:hidden" />
+
               <Link href={`/blog/${slug}`} passHref>
-                <h2 className="text-url mb-1 cursor-pointer text-xl font-semibold">
+                <h2 className="text-url mb-1 mt-4 cursor-pointer text-2xl font-semibold hover:underline md:mt-0 md:text-xl">
                   {title}
                 </h2>
               </Link>
 
-              <p className="mt-2 mb-3 line-clamp-3">{subtitle}</p>
+              <p className="mt-2 mb-3 ">{subtitle}</p>
 
               <div className="flex w-full justify-between">
-                <div className="flex flex-row items-center text-sm">
-                  <h4 className="mr-2 text-sm font-bold">Tags: </h4>
+                <div className=" hidden flex-row items-center text-sm md:flex">
+                  <h3 className="mr-2 text-sm font-bold">Tags: </h3>
                   {tags.map((tag) => (
                     <span
                       key={tag}
@@ -91,17 +138,25 @@ export async function getStaticProps() {
     // Remove the .md extension and use the file name as the slug
     const slug = fileName.replace(`.md`, ``);
 
-    // Read the raw content of the file and parse the frontmatter
+    // Read the raw content of the file and parse the frontMatter
     const rawFileContent = fs.readFileSync(`blog/${fileName}`, `utf-8`);
     const timeToRead = Math.ceil(wordsCount(rawFileContent) / 265);
 
-    const { data: frontmatter } = matter(rawFileContent);
+    const { data: frontMatter } = matter(rawFileContent);
 
     return {
       slug,
-      frontmatter,
+      frontMatter,
       timeToRead,
     };
+  });
+
+  // sort the posts by date in descending order
+  blogList.sort((a, b) => {
+    const a_date = dayjs(a.frontMatter.date, 'YYYY-MM-DD');
+    const b_date = dayjs(b.frontMatter.date, 'YYYY-MM-DD');
+
+    return b_date - a_date;
   });
 
   // Return the posts data to the page as props
