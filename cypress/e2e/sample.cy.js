608124e6a76b9fdab1105ba14fd5fa41f86fd238
EliKrasnoff <eli.krasnoff@gmail.com>
2022-07-13 14:06:08 -0700

    feat: ✨ creating cypress tests for each page

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,84 +1,56 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
-  });
-});
-
-describe('My First Test', () => {
+//visits homepage
+describe('Home page testing', () => {
   it('Visits fairdata', () => {
-    cy.visit('localhost:3000');
-  });
-});
-
-describe('My First Test', () => {
-  it('finds the content "type"', () => {
-    cy.visit('localhost:3000');
-
-    cy.contains('Contact us');
-  });
-});
-
-describe('My First Test', () => {
-  it('clicks the link "Contact us"', () => {
-    cy.visit('localhost:3000');
-
-    cy.contains('Contact us').click();
-  });
-});
-
-describe('My First Test', () => {
-  it('clicking "Contact us" navigates to a new url', () => {
-    cy.visit('localhost:3000');
-
-    cy.contains('Contact us').click();
-
-    // Should be on a new URL which includes '/commands/actions'
-    cy.url().should('include', '/contact-us');
-  });
-});
-
-describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('localhost:3000');
-
-    cy.contains('Contact us').click();
-
-    // Should be on a new URL which includes '/commands/actions'
-    cy.url().should('include', '/contact-us');
-
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('input:first')
-      .type('Testing name')
-      .should('have.value', 'Testing name');
-  });
-});
-
-// describe('My First Test', () => {
-//   it('clicking "type" shows the right headings', () => {
-//     cy.visit('https://example.cypress.io');
-
-//     cy.pause();
-
-//     cy.contains('type').click();
-
-//     // Should be on a new URL which includes '/commands/actions'
-//     cy.url().should('include', '/commands/actions');
-
-//     // Get an input, type into it and verify that the value has been updated
-//     cy.get('.action-email')
-//       .type('fake@email.com')
-//       .should('have.value', 'fake@email.com');
-//   });
-// });
-
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:3000'); // change URL to match your dev URL
-  });
-});
-
-describe('The Home Page', () => {
-  it('successfully loads', () => {
     cy.visit('/');
   });
 });
+
+//testing on the team page
+describe('Meet the Team page', () => {
+  it('Navigate to the team page', () => {
+    cy.visit('/');
+
+    cy.contains('Meet The Team').click();
+
+    cy.url().should('include', '/team');
+  });
+});
+
+//testing all project pages
+describe('Our projects pages', () => {
+  it('Navigate to SODA for SPARC page', () => {
+    cy.visit('/');
+
+    cy.contains('Our Projects').click();
+  })
+})
+
+//testing the contact us page
+describe('Contact Us Page', () => {
+  it('Navigate to contact us page and verify', () => {
+    cy.visit('/');
+
+    cy.contains('Contact us').click();
+
+    cy.url().should('include', '/contact-us');
+  });
+
+  it('Test form in Contact us page', () => {
+    cy.get('input[name="name"]')
+      .type('Testing name')
+      .should('have.value', 'Testing name');
+
+    cy.get('input[name="email"]')
+      .type("testing@gmail.com")
+      .should('have.value', 'testing@gmail.com');
+
+    cy.get('input[name="institution"]')
+      .type('Test Inc.')
+      .should('have.value', 'Test Inc.');
+
+    cy.get('textarea[name="message"]')
+      .type('This is a test for the contact page')
+      .should('have.value', 'This is a test for the contact page');
+  });
+});
+
