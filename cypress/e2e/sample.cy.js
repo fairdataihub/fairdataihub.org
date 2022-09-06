736d5c42f1325f5fa16741fcbc444bc04258686d
slugb0t <wheresdorian@gmail.com>
2022-09-06 13:00:57 -0700

    test: footer links being visited

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,17 +1,17 @@
 //visits homepage
 describe('Home page testing', () => {
-  it('Visits fairdata', () => {
-    cy.visit('/');
-  });
+  // it('Visits fairdata', () => {
+  //   cy.visit('/');
+  // });
 
-  it('Checks for swiper wrapper and marquee', () => {
-    cy.get('[id^=swiper-wrapper]').scrollIntoView();
-    for (let i = 0; i < 5; i++) {
-      cy.get('.swiper-button-next').click();
-      cy.wait(150);
-    }
-    cy.get('.marquee').should('have.css', 'animation');
-  });
+  // it('Checks for swiper wrapper and marquee', () => {
+  //   cy.get('[id^=swiper-wrapper]').scrollIntoView();
+  //   for (let i = 0; i < 5; i++) {
+  //     cy.get('.swiper-button-next').click();
+  //     cy.wait(150);
+  //   }
+  //   cy.get('.marquee').should('have.css', 'animation');
+  // });
 
   it('Checks footer links', () => {
     cy.visit('/');
@@ -19,43 +19,29 @@ describe('Home page testing', () => {
 
     cy.get('#fairdata-footer').scrollIntoView();
     //check links within the footer page
-    let test = cy.get('#footer-team');
-    cy.log(test.children);
 
-    // cy.get('#footer-team')
-    //   .children[0].invoke('attr', 'href')
-    //   .then((href) => {
-    //     cy.visit(href);
-    //   });
-
-    // cy.visit('/');
-    // cy.wait(200);
-
-    // cy.get('#footer-blog')
-    //   .children[0].invoke('attr', 'href')
-    //   .then((href) => {
-    //     cy.visit(href);
-    //   });
-
-    // cy.visit('/');
-    // cy.wait(200);
-
-    // cy.get('#footer-contact-us')
-    //   .children[0].invoke('attr', 'href')
-    //   .then((href) => {
-    //     cy.visit(href);
-    //   });
-
-    // cy.visit('/');
-    // cy.wait(200);
-
-    // cy.get('#footer-contact-us')
-    //   .children[0].invoke('attr', 'href')
-    //   .then((href) => {
-    //     cy.visit(href);
-    //   });
-    // cy.visit('/');
-    // cy.wait(200);
+    // cy.log(elem);
+    cy.get('#footer-links-container')
+      .children()
+      .then((elem) => {
+        //three footer containers holding links
+        for (let i = 0; i < elem.length; i++) {
+          //footer element containers
+          cy.get(elem[i].children[1]).then((element) => {
+            element.children().each((index, link) => {
+              //a tag is found here
+              cy.log(link.children[0]);
+              cy.get(link.children[0])
+                .invoke('attr', 'href')
+                .then((href) => {
+                  cy.log(href);
+                  cy.visit(href);
+                  cy.wait(200);
+                });
+            });
+          });
+        }
+      });
   });
 });
 
