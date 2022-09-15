f9e860917acabaad06a889e5dade17f526d1de40
Sanjay Soundarajan <sanjay.16@live.com>
2022-09-15 16:10:20 -0700

    chore: 🔧 remove duplicate snippet

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -57,24 +57,6 @@ describe('My First Test', () => {
   });
 });
 
-// describe('My First Test', () => {
-//   it('clicking "type" shows the right headings', () => {
-//     cy.visit('https://example.cypress.io');
-
-//     cy.pause();
-
-//     cy.contains('type').click();
-
-//     // Should be on a new URL which includes '/commands/actions'
-//     cy.url().should('include', '/commands/actions');
-
-//     // Get an input, type into it and verify that the value has been updated
-//     cy.get('.action-email')
-//       .type('fake@email.com')
-//       .should('have.value', 'fake@email.com');
-//   });
-// });
-
 describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('http://localhost:3000'); // change URL to match your dev URL
