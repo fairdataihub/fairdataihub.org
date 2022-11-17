9c3b143286fbe3e314aa1a82d3e3dd5be36a43e2
Sanjay Soundarajan <sanjay.16@live.com>
2022-11-17 14:54:19 -0800

    chore: 🔨 update element names

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -4,7 +4,7 @@ import ContactForm from '@/components/contact-us/contactForm';
 
 export default function ContactUs() {
   return (
-    <div>
+    <>
       <Head>
         <title>Contact Us - Fair Data Innovations Hub</title>
         <meta
@@ -46,34 +46,32 @@ export default function ContactUs() {
         />
       </Head>
 
-      <main>
-        <div className="relative flex items-center justify-center bg-transparent bg-cover bg-no-repeat ">
-          <div className="area absolute bottom-0 z-0">
-            <ul className="circles">
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-
-          <section className="z-10 py-8">
-            <div className="container mx-auto w-screen max-w-screen-md rounded-lg bg-gray-50 px-10 py-12 shadow-lg">
-              <h2 className="pt-3 pb-5 text-center text-3xl font-semibold sm:text-4xl">
-                Let us know if you have any feedback or want to collaborate
-              </h2>
-
-              <ContactForm />
-            </div>
-          </section>
+      <div className="relative flex items-center justify-center bg-transparent bg-cover bg-no-repeat ">
+        <div className="area absolute bottom-0 z-0">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
-      </main>
-    </div>
+
+        <section className="z-10 py-8">
+          <div className="container mx-auto w-screen max-w-screen-md rounded-lg bg-gray-50 px-10 py-12 shadow-lg">
+            <h2 className="pt-3 pb-5 text-center text-3xl font-semibold sm:text-4xl">
+              Let us know if you have any feedback or want to collaborate
+            </h2>
+
+            <ContactForm />
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
