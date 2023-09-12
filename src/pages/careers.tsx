1aee6be5b3c4fea0b1fca9bbb4947defd3d60056
slugb0t <wheresdorian@gmail.com>
2023-09-11 22:22:54 -0700

    feat: ✨ create base for careers page

---
+++ src/pages/careers.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+
+export default function CookiePolicy() {
+  return (
+    <>
+      <Head>
+        <title>Cookie Policy - Fair Data Innovations Hub</title>
+        <meta
+          property="og:title"
+          content="Careers - Fair Data Innovations Hub"
+        />
+        <meta
+          name="twitter:title"
+          content="Careers - Fair Data Innovations Hub"
+        />
+
+        <link rel="canonical" href="https://fairdataihub.org/careers" />
+        <meta property="og:url" content="https://fairdataihub.org/careers" />
+        <meta name="twitter:url" content="https://fairdataihub.org/careers" />
+
+        <meta name="description" content="View careers of fairdataihub.org" />
+        <meta
+          property="og:description"
+          content="View careers of fairdataihub.org"
+        />
+        <meta
+          name="twitter:description"
+          content="View careers of fairdataihub.org"
+        />
+
+        <meta
+          property="og:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://fairdataihub.org/thumbnails/index.png"
+        />
+      </Head>
+
+      <section>test</section>
+    </>
+  );
+}
