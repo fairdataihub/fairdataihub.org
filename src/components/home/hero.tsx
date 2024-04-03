034882d6d6882ecd773cf99dda59a80a5962b522
Sanjay Soundarajan <sanjay.16@live.com>
2024-04-02 17:41:41 -0700

    wip: 🚧 add projects grid

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -6,46 +6,35 @@ import heroLottie from '../../assets/lotties/hero.json';
 
 export default function Hero() {
   return (
-    <section className="hero">
-      <div className="container mx-auto max-w-screen-lg px-6 py-8">
-        <div className="items-center justify-center md:flex">
-          <div className="w-full p-2 lg:w-1/2 lg:max-w-lg">
-            <h1 className="w-full text-left text-4xl font-black sm:text-3xl md:py-3 md:text-4xl lg:text-5xl">
-              Making FAIR
-              <span className="text-accent"> data </span>
-              practices more accessible
-            </h1>
+    <div className="hero container mx-auto max-w-screen-xl px-6 py-8 items-center justify-center md:flex">
+      <div className="w-full p-2 lg:w-1/2 lg:max-w-lg">
+        <h1 className="w-full text-left text-4xl font-black sm:text-3xl md:py-3 md:text-4xl lg:text-5xl">
+          Simplifying data sharing to propel AI-driven discoveries
+        </h1>
 
-            <p className="mt-2 w-full text-xl font-normal text-black sm:mt-0 sm:text-base md:text-lg lg:text-xl">
-              <span className="font-medium">F</span>indable,&nbsp;
-              <span className="font-medium">A</span>ccessible,&nbsp;
-              <span className="font-medium">I</span>nteroperable, and&nbsp;
-              <span className="font-medium">R</span>eusable (FAIR) data
-              practices are taking the biomedical research world by storm. We
-              build open source tools to help researchers navigate through them.
-            </p>
+        <p className="mt-2 w-full text-xl font-normal text-black sm:mt-0 sm:text-base md:text-lg lg:text-xl">
+          Availability of biomedical research data is critical to fuel AI-driven
+          discoveries. We develop open source tools, standards, and guidelines
+          that support biomedical researchers in preparing and sharing data and
+          other research outcomes such that they are optimally reusable by both
+          humans and machines, including AI and ML models.
+        </p>
 
-            <Link href="/contact-us" passHref>
-              <button
-                type="button"
-                className="my-3 rounded-lg border-2 border-black bg-transparent p-3 text-center text-base font-semibold text-black transition-all hover:border-light-accent hover:text-accent"
-                data-umami-event="Home page link"
-                data-umami-event-value="Contact us"
-              >
-                Contact us
-              </button>
-            </Link>
-          </div>
-
-          <div className="mt-6 flex w-full items-center justify-center p-5 lg:mt-0 lg:w-1/2 lg:p-2">
-            <LottieAnimation
-              animationData={heroLottie}
-              width={400}
-              height={400}
-            />
-          </div>
-        </div>
+        <Link href="/contact-us" passHref>
+          <button
+            type="button"
+            className="my-3 rounded-lg border-2 border-black bg-transparent p-3 text-center text-base font-semibold text-black transition-all hover:border-light-accent hover:text-accent hidden"
+            data-umami-event="Home page link"
+            data-umami-event-value="Contact us"
+          >
+            Contact us
+          </button>
+        </Link>
       </div>
-    </section>
+
+      <div className="mt-6 flex w-full items-center justify-center p-5 lg:mt-0 lg:w-1/2 lg:p-2">
+        <LottieAnimation animationData={heroLottie} width={400} height={400} />
+      </div>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import AboutUs from '@/components/home/aboutUs';
 import Collaborators from '@/components/home/collaborators';
 import Hero from '@/components/home/hero';
 import ProjectsCarousel from '@/components/home/projectsCarousel';
-import Vision from '@/components/home/vision';
+// import Vision from '@/components/home/vision';
 import Seo from '@/components/seo/seo';
 
 export default function Home() {
@@ -13,15 +14,60 @@ export default function Home() {
         templateImage="https://fairdataihub.org/thumbnails/index.png"
       />
 
-      <div className="bg-haikeiWavy bg-cover bg-top bg-no-repeat md:bg-right-top 2xl:bg-contain">
+      <div className="relative">
         <section className="mb-10 pt-12 sm:pt-16">
           <Hero />
         </section>
 
-        <section className="py-10">
-          <Vision />
-        </section>
+        <div
+          className="absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="relative aspect-square rotate-[30deg] bg-gradient-to-tr from-pink-300 to-purple-500 opacity-30"
+            style={{
+              clipPath: `polygon(6% 8%, 52% 29%, 93% 51%, 68% 82%, 99% 94%, 32% 57%, 48% 10%, 5% 37%, 69% 52%, 63% 63%, 81% 28%, 56% 64%, 62% 21%, 46% 61%, 79% 65%, 93% 68%, 88% 75%)`,
+            }}
+          ></div>
+        </div>
       </div>
+
+      <section className="py-10 mx-auto max-w-screen-xl">
+        <div className="flex gap-3 items-center">
+          <div className="bg-white w-3/5 flex h-full flex-col border rounded-md border-slate-200">
+            <div className="h-[280px]">
+              <img
+                src="https://medusajs.com/images/image%20(13)-fd0d0376.png"
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div className="px-3 py-4">
+              <h3>Project 1</h3>
+            </div>
+          </div>
+
+          <div className="bg-white w-2/5 flex h-full flex-col border rounded-md border-slate-200">
+            <div className="h-[280px]">
+              <img
+                src="https://medusajs.com/images/image%20(7)-df85652f.png"
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div className="px-3 py-4">
+              <h3>Project 2</h3>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </section>
+
+      {/* <section className="py-10">
+        <Vision />
+      </section> */}
 
       <section className="bg-gray-50 py-10">
         <AboutUs />
