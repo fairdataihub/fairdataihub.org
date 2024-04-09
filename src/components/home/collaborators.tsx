f04b013ad72ec9bcec1ed2173adedb8a05d4fc92
Sanjay Soundarajan <sanjay.16@live.com>
2024-04-09 15:44:33 -0700

    fix: 🐛 split background

--- src/components/home/collaborators.tsx
+++ src/components/home/collaborators.tsx
@@ -39,7 +39,7 @@ export default function Collaborators() {
     <section>
       <div className="container mx-auto max-w-screen-xl px-6 pb-0 pt-4 md:p-10">
         <div className="mb-8 flex w-full flex-col items-center">
-          <p className="header-gradient-background my-2 text-center text-4xl font-extrabold sm:text-4xl">
+          <p className="my-2 text-center text-4xl font-extrabold text-black sm:text-4xl">
             Where do our tools make a difference?
           </p>
         </div>
