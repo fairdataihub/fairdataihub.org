36a11d6868f5f3a9fca925fab84934be3943520b
Paapakane <pmensahkane@health.ucsd.edu>
2024-09-14 20:03:26 -0500

    Update team.tsx
    Added Paapa to the team

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -196,6 +196,33 @@ const TEAM_JSON = [
     },
   },
   {
+    id: `Paapa-Mensah-Kane`,
+    name: `Paapa Mensah-Kane (he/him)`,
+    title: `Pharmacologist`,
+    bio: `Paapa is originally from Ghana where he had his Pharmacy and Master's degree. He also holds a PhD degree in pharmacolgy and neuroscience from UNTHSC, Texas. He became a part of the team in 2024. His interest lies in pharmacometrics and computational pharmacology. In his leisure time, he watches soccer (REAL MADRID).`,
+    image: `/images/people/paapa-full.jpg`,
+    width: 795,
+    height: 718,
+    borderTop: true,
+    borderBottom: false,
+    education: [
+      `Ph.D in Pharmacology and Neuroscience (University of North Texas Health Science Center, Fort Worth, 2024)`,
+      `M.Sc. Natural Products Pharmacology (Kwame Nkrumah University of Science and Technology, Kumasi, 2015)`,
+    ],
+    twitter: {
+      show: false,
+      link: `https://twitter.com`,
+    },
+    github: {
+      show: true,
+      link: `https://github.com/Paapakane`,
+    },
+    linkedin: {
+      show: true,
+      link: `https://www.linkedin.com/in/paapa-mensah-kane-bpharm-mphil-ph-d-a58765133/`,
+    },
+  },
+  {
     id: `Krista-Scrivner`,
     name: `Krista Scrivner (she/her)`,
     title: `Contract and Grant Manager`,
