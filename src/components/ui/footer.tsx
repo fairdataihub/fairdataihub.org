60003b3253f2e805f1076f7335120547c4835a2d
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2026-04-29 09:08:50 -0700

    feat: add gitlab to footer links

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
               Helping researchers navigate the world of FAIR practices.
             </p>
 
-            <div className="mt-2 flex flex-row gap-4">
+            <div className="mt-2 flex flex-row gap-3">
               {/* <Link
                 href="https://www.twitter.com/fairdataihub"
                 className="group rounded-md p-1 transition outline-none hover:text-pink-500 focus-visible:ring-2 focus-visible:ring-pink-400/60"
@@ -99,6 +99,29 @@ export default function Footer() {
                   aria-hidden="true"
                 >
                   <path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path>
+                </svg>
+              </Link>
+
+              <Link
+                href="https://gitlab.com/fairdataihub"
+                target="_blank"
+                className="group rounded-md p-1 transition outline-none hover:text-pink-500 focus-visible:ring-2 focus-visible:ring-pink-400/60"
+                data-umami-event="Navigation link"
+                data-umami-event-location="Footer"
+                data-umami-event-value="GitLab"
+                aria-label="GitLab"
+                rel="noopener"
+              >
+                <svg
+                  className="h-6 w-6 fill-current text-gray-400 transition group-hover:text-pink-500"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="m21.663 9.987l-.028-.072l-2.719-7.094a.71.71 0 0 0-.706-.449a.71.71 0 0 0-.654.522L15.72 8.52H8.282L6.443 2.895a.71.71 0 0 0-.652-.524a.72.72 0 0 0-.707.45L2.362 9.925l-.028.07a5.06 5.06 0 0 0 1.674 5.838l.01.007l.024.019l4.147 3.104l2.05 1.553l1.247.944a.84.84 0 0 0 1.016 0l1.247-.944l2.05-1.553l4.172-3.123l.01-.008a5.055 5.055 0 0 0 1.682-5.845"
+                  />
                 </svg>
               </Link>
             </div>
